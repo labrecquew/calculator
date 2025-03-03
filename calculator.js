64adb29933d01d90cb6ec4fd4b1a46f8c538a087
labrecquew <labrecque.weston@gmail.com>
2025-03-03 09:57:23 -0500

    Create the basic functions of the calculator
    These functions were add, subtract, multiply, divide, and an operation
    function that can recognize what operater means what function

---
+++ calculator.js
@@ -0,0 +1,39 @@
+let numOne = 0;
+let numTwo = 0;
+let operater = '';
+
+
+function operate(num1, num2, operation) {
+    let result = null;
+    switch(operation) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
